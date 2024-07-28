0406badb2f728b91c20c5e6a80bc97ec654e0a98
Andrew Xu <xusandrew@gmail.com>
2024-07-28 13:33:57 -0400

    Refactor Entity manager

--- entities/entity_manager.cc
+++ entities/entity_manager.cc
@@ -1,1 +1,36 @@
+#include "entity_manager.h"
+#include "entity.h"
+#include "component.h"
+#include "position_component.h"
 
+std::shared_ptr<Entity> EntityManager::createEntity()
+{
+    auto entity = std::make_shared<Entity>();
+    entities.push_back(entity);
+    return entity;
+}
+
+void EntityManager::removeEntity(std::shared_ptr<Entity> entity)
+{
+    // moves all elements equal to entity to the end of the vector and returns an iterator to the new end of the vector, then erase
+    entities.erase(std::remove(entities.begin(), entities.end(), entity), entities.end());
+}
+
+std::shared_ptr<Entity> EntityManager::getEntity(int row, int col)
+{
+    for (auto entity : entities)
+    {
+        auto position_component = entity->getComponent<PositionComponent>();
+
+        if (!position_component)
+        {
+            continue;
+        }
+
+        if (position_component->row == row && position_component->col == col)
+        {
+            return entity;
+        }
+    }
+    return nullptr;
+}
--- entities/entity_manager.h
+++ entities/entity_manager.h
@@ -2,11 +2,10 @@
 #include <memory>
 #include <utility>
 #include <algorithm>
-#include "entity.h"
-#include "component.h"
-#include "position_component.h"
 
 class Entity;
+class Component;
+class PositionComponent;
 
 class EntityManager
 {
@@ -14,35 +13,7 @@ private:
     std::vector<std::shared_ptr<Entity>> entities;
 
 public:
-    std::shared_ptr<Entity> createEntity()
-    {
-        auto entity = std::make_shared<Entity>();
-        entities.push_back(entity);
-        return entity;
-    }
-
-    void removeEntity(std::shared_ptr<Entity> entity)
-    {
-        // moves all elements equal to entity to the end of the vector and returns an iterator to the new end of the vector, then erase
-        entities.erase(std::remove(entities.begin(), entities.end(), entity), entities.end());
-    }
-
-    std::shared_ptr<Entity> getEntity(int row, int col)
-    {
-        for (auto entity : entities)
-        {
-            auto position_component = entity->getComponent<PositionComponent>();
-
-            if (!position_component)
-            {
-                continue;
-            }
-
-            if (position_component->row == row && position_component->col == col)
-            {
-                return entity;
-            }
-        }
-        return nullptr;
-    }
+    std::shared_ptr<Entity> createEntity();
+    void removeEntity(std::shared_ptr<Entity> entity);
+    std::shared_ptr<Entity> getEntity(int row, int col);
 };

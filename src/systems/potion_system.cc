8eb38d0986290ed9ffbaa69cff08986bc4409aee
Andrew Xu <xusandrew@gmail.com>
2024-07-30 01:19:58 -0400

    Item system

--- src/systems/potion_system.cc
+++ src/systems/potion_system.cc
@@ -51,54 +51,54 @@ void PotionSystem::usePotion(EntityManager &entityManager, std::shared_ptr<Entit
 void PotionSystem::update(EntityManager &entityManager, std::shared_ptr<Entity> player)
 {
     std::shared_ptr<ActionComponent> actionComponent = player->getComponent<ActionComponent>();
-    if (actionComponent->use)
+    if (!actionComponent->use)
+        return;
+
+    std::shared_ptr<PositionComponent> positionComponent = player->getComponent<PositionComponent>();
+    std::shared_ptr<Entity> potion;
+
+    std::string &direction = player->getComponent<DirectionComponent>()->direction;
+    if (direction == "no")
     {
-        std::shared_ptr<PositionComponent> positionComponent = player->getComponent<PositionComponent>();
-        std::shared_ptr<Entity> potion;
-
-        std::string &direction = player->getComponent<DirectionComponent>()->direction;
-        if (direction == "no")
-        {
-            potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col);
-        }
-        else if (direction == "so")
-        {
-            potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col);
-        }
-        else if (direction == "ea")
-        {
-            potion = entityManager.getEntity(positionComponent->row, positionComponent->col + 1);
-        }
-        else if (direction == "we")
-        {
-            potion = entityManager.getEntity(positionComponent->row, positionComponent->col - 1);
-        }
-        else if (direction == "ne")
-        {
-            potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col + 1);
-        }
-        else if (direction == "nw")
-        {
-            potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col - 1);
-        }
-        else if (direction == "se")
-        {
-            potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col + 1);
-        }
-        else if (direction == "sw")
-        {
-            potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col - 1);
-        }
-
-        if (!potion)
-        {
-            return;
-        }
-
-        if (potion->getComponent<PotionTypeComponent>())
-        {
-            return;
-        }
-        usePotion(entityManager, player, potion);
+        potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col);
     }
+    else if (direction == "so")
+    {
+        potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col);
+    }
+    else if (direction == "ea")
+    {
+        potion = entityManager.getEntity(positionComponent->row, positionComponent->col + 1);
+    }
+    else if (direction == "we")
+    {
+        potion = entityManager.getEntity(positionComponent->row, positionComponent->col - 1);
+    }
+    else if (direction == "ne")
+    {
+        potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col + 1);
+    }
+    else if (direction == "nw")
+    {
+        potion = entityManager.getEntity(positionComponent->row - 1, positionComponent->col - 1);
+    }
+    else if (direction == "se")
+    {
+        potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col + 1);
+    }
+    else if (direction == "sw")
+    {
+        potion = entityManager.getEntity(positionComponent->row + 1, positionComponent->col - 1);
+    }
+
+    if (!potion)
+    {
+        return;
+    }
+
+    if (!potion->getComponent<PotionTypeComponent>())
+    {
+        return;
+    }
+    usePotion(entityManager, player, potion);
 }

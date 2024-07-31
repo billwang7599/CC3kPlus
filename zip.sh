8c5900f9b29de113f7bfc677ef94a282ccc2e5f5
Andrew Xu <xusandrew@gmail.com>
2024-07-30 23:03:57 -0400

    Add uml diagram

--- zip.sh
+++ zip.sh
@@ -5,6 +5,6 @@ output_zip="cc3k.zip"
 
 
 # Zip all files in the directory
-zip -r "$output_zip" . -x "*.git*" -x "*.DS_Store" -x "*.zip" -x "*.sh" -x "*.md" -x "*.pdf" -x "*.png" -x "*.jpg" -x "*.jpeg" -x "*.gif" -x "*.bmp" -x "*.tiff" -x "*.tif" -x "*.ico" -x ".vscode/*"
+zip -r "$output_zip" . -x "*.git*" -x "*.DS_Store" -x "*.zip" -x "*.sh" -x "*.md" -x "cc3k+.pdf" -x "project_guidelines.pdf" -x "*.png" -x "*.jpg" -x "*.jpeg" -x "*.gif" -x "*.bmp" -x "*.tiff" -x "*.tif" -x "*.ico" -x ".vscode/*"
 
 echo "Files zipped successfully!"
